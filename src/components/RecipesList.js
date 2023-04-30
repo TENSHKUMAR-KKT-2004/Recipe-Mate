490bd16cd4b3fb4915795a5c73b146e56fffbff1
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-04-30 22:50:32 +0530

    adding delete functionality for all recipes

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,12 +1,19 @@
 import { Link } from 'react-router-dom';
 import './RecipesList.css'
 import { useTheme } from '../hooks/useTheme';
+import {firestoreDB} from '../firebase/config'
+
+import Trashcan from '../assets/delete_icon.svg'
 
 const RecipesList = ({ recipes }) => {
     const {mode} = useTheme()
     
     if (recipes.length === 0){
         return <div className='error'>No Recipes to Load...</div>
+    }
+
+    const handleClick = (id)=>{
+        firestoreDB.collection('recipes').doc(id).delete()
     }
 
     return (
@@ -18,6 +25,12 @@ const RecipesList = ({ recipes }) => {
                         <p>{recipe.cookingTime} to make.</p>
                         <div>{recipe.method.substring(0,100)}...</div>
                         <Link to={`/recipe/${recipe.id}`}>Cook this</Link>
+                        <img 
+                        className='delete'
+                        src={Trashcan} 
+                        alt="delete-icon" 
+                        onClick={()=>{handleClick(recipe.id)}}
+                        />
                     </div>
                 ))
             }
