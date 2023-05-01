4c914a00b4049921c41debafc3554f014f0828a2
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-05-01 13:32:07 +0530

    adding searching functionality with firestore

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,25 +1,55 @@
-import {useSearchParams} from 'react-router-dom'
-import {useFetch} from '../../hooks/useFetch'
-
+import { useSearchParams } from 'react-router-dom'
+import { firestoreDB } from '../../firebase/config'
 import RecipesList from '../../components/RecipesList'
 
 import './Search.css'
+import { useEffect,useState } from 'react'
 
 const Search = () => {
     const [queryString] = useSearchParams()
-    const query = queryString.get('q')
+    const searchTerm = queryString.get('q')
 
-    const url = 'http://localhost:3004/recipes?q='+query
-    const {data:result,error,isPending} = useFetch(url)
+    const [result, setResult] = useState([]);
+    const [isPending, setPending] = useState(false);
+    const [error, setError] = useState(null);
 
-    return ( <div>
+    useEffect(() => {
+        setPending(true)
+
+        let query = firestoreDB.collection('recipes')
+        if (searchTerm) {
+            // split the search term into individual words
+            const searchTerms = searchTerm.split(' ');
+
+            // construct a query for each individual word in the search term
+            searchTerms.forEach(term => {
+                query = query.where('title', '>=', term).where('title', '<=', term + '\uf8ff');
+            })
+        }
+        query.get()
+            .then((querySnapshot) => {
+                const data = querySnapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }))
+                setResult(data)
+                setPending(false)
+            })
+            .catch((err) => {
+                setError(err.message)
+                setPending(false)
+            })
+
+    }, [searchTerm])
+
+    return (<div>
         <h2 className="page-title">
-            Recipes including "{query}"
+            Recipes including "{searchTerm}"
         </h2>
-        {error && <p className="error">{error}</p> }
-        {isPending && <p className="loading">Loading...</p> }
-        {result && <RecipesList recipes={result} /> }
-    </div> );
+        {error && <p className="error">{error}</p>}
+        {isPending && <p className="loading">Loading...</p>}
+        {result && <RecipesList recipes={result} />}
+    </div>)
 }
- 
+
 export default Search;
